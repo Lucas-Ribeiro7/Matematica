234cf74f574b318e2931d8bbaa22bb3eb7d823a1
Lucas Ribeiro <caetanolucasribeiro@gmail.com>
2020-09-20 13:19:38 -0300

    Update

--- Código Fonte/calcular.php
+++ Código Fonte/calcular.php
@@ -7,25 +7,27 @@
     <title>Matemática</title>
 </head>
 <body background="Imagens/Matematica.jpg" bgproperties="fixed"> 
-    <?php
-        include_once 'funcoes.php';
-            $num = $_POST['num'];
-            $escolha = $_POST['escolha'];
+    <div class="borda">
+        <?php
+            include_once 'funcoes.php';
+                $num = $_POST['num'];
+                $escolha = $_POST['escolha'];
 
-            switch($escolha){
-                case 'posit_negati' :
-                    posi_negat($num);
-                break;
-                case 'par_impar' :
-                    par_impar($num);
-                break;
-                case 'tabuada' :
-                    tabuada($num);
-                break;
-                case 'raiz-quadrada' :
-                    raizQuadrada($num);
-                break;
-            }
-    ?>
+                switch($escolha){
+                    case 'posit_negati' :
+                        posi_negat($num);
+                    break;
+                    case 'par_impar' :
+                        par_impar($num);
+                    break;
+                    case 'tabuada' :
+                        tabuada($num);
+                    break;
+                    case 'raiz-quadrada' :
+                        raizQuadrada($num);
+                    break;
+                }
+        ?>
+    </div>
 </body>
 </html>
